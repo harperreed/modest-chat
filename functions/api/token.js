3b7435b54e069dd9153ba675f61fe1001bff4f9f
Harper Reed <harper@nata2.org>
2020-03-14 12:12:45 -0500

    made token function be auth only

--- functions/api/token.js
+++ functions/api/token.js
@@ -32,10 +32,56 @@ const MAX_ALLOWED_SESSION_DURATION = 14400;
  *
  */ 
 
-exports = module.exports = functions.https.onRequest((request, response) => {
+exports = module.exports = functions.https.onRequest(async(request, response) => {
 
-    var identity = request.query.identity || randomName();
+    
     var room_name = request.query.room_name;
+    var identity = request.query.identity || randomName();
+    
+    //test bearer function
+
+    if ((!request.headers.authorization || !request.headers.authorization.startsWith('Bearer ')) &&
+        !(request.cookies && request.cookies.__session)) {
+      console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+          'Make sure you authorize your request by providing the following HTTP header:',
+          'Authorization: Bearer <Firebase ID Token>',
+          'or by passing a "__session" cookie.');
+      response.status(403).send('Unauthorized');
+      return;
+    }
+
+    let idToken;
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer ')) {
+      console.log('Found "Authorization" header');
+      // Read the ID Token from the Authorization header.
+      idToken = request.headers.authorization.split('Bearer ')[1];
+    } else if(request.cookies) {
+      console.log('Found "__session" cookie');
+      // Read the ID Token from cookie.
+      idToken = request.cookies.__session;
+    } else {
+      // No cookie
+      response.status(403).send('Unauthorized');
+      return;
+    }
+
+
+    /* 
+    try {
+      const decodedIdToken = await admin.auth().verifyIdToken(idToken)
+      console.log('ID Token correctly decoded', decodedIdToken);
+
+    } catch (error) {
+      console.error('Error while verifying Firebase ID token:', error);
+      response.status(403).send('Unauthorized');
+      return;
+    }
+    */
+    
+    
+
+
+
 
     // Create an access token which we will sign and return to the client,
     // containing the grant we just created.
